09a244aedef4bab33b488905f9a828f811949177
bxi4 <muhammedaahaj@gmail.com>
2022-03-12 23:38:39 +0300

    add menu option

--- app/src/main/java/com/example/project/Welcome.java
+++ app/src/main/java/com/example/project/Welcome.java
@@ -1,10 +1,16 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,9 +36,39 @@ public class Welcome extends AppCompatActivity {
                 edit.commit();
                 startActivity(new Intent(Welcome.this,MainActivity.class));
 
-
-
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater m = getMenuInflater();
+        m.inflate(R.menu.mym,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+//option menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getTitle().equals("Exit")){
+            AlertDialog.Builder b = new AlertDialog.Builder(Welcome.this);
+            b.setMessage("do you want Exit ? ");
+            b.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            b.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            AlertDialog a = b.create();
+            a.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
